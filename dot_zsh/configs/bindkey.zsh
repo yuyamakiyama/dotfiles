7ea2cf1e1bc430f033beb52562eab9a24f3005b3
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-03-03 07:13:27 +0900

    Update .zsh/configs/bindkey.zsh

--- dot_zsh/configs/bindkey.zsh
+++ dot_zsh/configs/bindkey.zsh
@@ -7,7 +7,7 @@ bindkey '\e[27;2;13~' insert-newline
 # Cursor / VSCode (via keybindings.json sendSequence)
 bindkey '\e[13;2u' insert-newline
 
-# Resize cmux panes (Cmd+Shift+Arrow)
+# Resize cmux panes (Ctrl+Cmd+Arrow)
 # Moves the divider in the arrow direction. If the focused pane has no border
 # on that side, resizes the neighbor pane instead.
 function _cmux-resize() {
@@ -17,6 +17,7 @@ function _cmux-resize() {
     local other=$(cmux list-panes 2>/dev/null | grep -v '\[focused\]' | grep -o 'pane:[0-9]*' | head -1)
     [[ -n $other ]] && cmux resize-pane --pane "$other" -$dir --amount 150 2>/dev/null
   fi
+  return 0
 }
 function cmux-resize-left()  { _cmux-resize L }
 function cmux-resize-right() { _cmux-resize R }
@@ -26,7 +27,7 @@ zle -N cmux-resize-left
 zle -N cmux-resize-right
 zle -N cmux-resize-up
 zle -N cmux-resize-down
-bindkey '\e[1;10D' cmux-resize-left
-bindkey '\e[1;10C' cmux-resize-right
-bindkey '\e[1;10A' cmux-resize-up
-bindkey '\e[1;10B' cmux-resize-down
+bindkey '\e[1;13D' cmux-resize-left
+bindkey '\e[1;13C' cmux-resize-right
+bindkey '\e[1;13A' cmux-resize-up
+bindkey '\e[1;13B' cmux-resize-down
