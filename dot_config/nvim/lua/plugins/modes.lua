9fcae099c682df2b531742e6f4421ccfa02aab4d
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-03-07 18:18:51 -0500

    Add .config/nvim/lua/plugins/modes.lua

---
+++ dot_config/nvim/lua/plugins/modes.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "mvllow/modes.nvim",
+    event = "VeryLazy",
+    opts = {
+      colors = {
+        insert = "#78cec0",
+        visual = "#f6c177",
+        delete = "#eb6f92",
+        copy = "#c4a7e7",
+      },
+      line_opacity = 0.2,
+    },
+  },
+}
