7418e410042b55f234ea1af98297941ce5c9727d
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-03-08 23:51:02 -0400

    Update .config/nvim/lua/config/options.lua

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,3 +3,8 @@
 -- Add any additional options here
 
 vim.opt.spell = false
+
+-- Brighter cursor colors
+vim.api.nvim_set_hl(0, "Cursor", { fg = "#1a1b26", bg = "#c0caf5" })
+vim.api.nvim_set_hl(0, "iCursor", { fg = "#1a1b26", bg = "#9ece6a" })
+vim.opt.guicursor = "n-v-c:block-Cursor,i-ci-ve:ver25-iCursor,r-cr-o:hor20-Cursor"
