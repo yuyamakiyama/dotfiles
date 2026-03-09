091acf516a5321ca16a2eb9bfe72e9d3b450d68b
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-03-09 03:34:46 -0400

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,16 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto-save on text change and leaving insert mode
+vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
+  callback = function(event)
+    local buf = event.buf
+    if vim.bo[buf].modified and vim.bo[buf].buftype == "" and vim.fn.expand("%") ~= "" then
+      vim.api.nvim_buf_call(buf, function()
+        require("conform").format({ buf = buf, async = false, lsp_fallback = true })
+        vim.cmd("silent! write")
+      end)
+    end
+  end,
+})
