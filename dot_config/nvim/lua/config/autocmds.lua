b82d390abe0b2b4068b2ab7a37ab19a6bdebb289
Yuya Makiyama <yuya.makiyama@gmail.com>
2026-03-12 23:12:41 -0400

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -5,7 +5,7 @@
 -- with `vim.api.nvim_create_autocmd`
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
 -- Disable LSP inlay hints on attach
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -13,4 +13,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.lsp.inlay_hint.enable(false)
   end,
 })
-
